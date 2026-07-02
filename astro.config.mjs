5d32c41a9a28807300faf2a89334ff2fa0ae75f1
byron1001 <gradine702002@gmail.com>
2026-07-02 10:38:14 +0800

    deployment on github

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://byron1001.github.io",
+  base: "/firm-demo",
   vite: {
     plugins: [tailwindcss()]
   },
